4aa1ac9faa8beed5104d6cdea5e73b239316b8d7
Bart Römgens <bart.romgens@gmail.com>
2024-06-02 14:48:29 +0200

    Add batch get (tiles) of buildings from overpass API

--- getbuildings.py
+++ getbuildings.py
@@ -5,15 +5,18 @@ See https://github.com/mvexel/overpass-api-python-wrapper for the Python wrapper
 """
 
 import json
+import time
 from dataclasses import dataclass
 from functools import partial
 from typing import Any
 from typing import Dict
 from typing import List
 
+import geojson
 import pyproj
 import requests
 import overpass
+import shapely.geometry
 from shapely.geometry import Point
 from shapely.geometry import Polygon
 from shapely.ops import transform
@@ -77,10 +80,21 @@ class Building:
 
 
 def main():
-    response = get_buildings_test_area()
-    print(json.dumps(response, indent=2))
+    test_bbox = (
+        52.09261214198491,
+        5.562860339734603,
+        52.109061883798304,
+        5.592364638898419,
+    )
+    test_box_large = (
+        51.998199003792266,
+        5.337630742650022,
+        52.261223462827274,
+        5.809699529271116,
+    )
+    buildings_raw = get_buildings_batches(test_box_large)
+    # print(json.dumps(buildings_raw, indent=2))
 
-    buildings_raw = response["elements"]
     buildings: List[Building] = []
     for building_raw in buildings_raw:
         buildings.append(Building.create_from_osm_way(building_raw))
@@ -93,6 +107,18 @@ def main():
     print(f"area: {test_building.area_square_meters} m^2")
     print(f"length width: {test_building.length_width} m")
 
+    min_area = 200
+    polygons = [
+        building.polygon
+        for building in buildings
+        if building.area_square_meters > min_area
+    ]
+    geojson_feature_collection = shapely_collection_to_geojson(polygons)
+    print(geojson.dumps(geojson_feature_collection, indent=2))
+
+    with open("buildings.geojson", "w") as outfile:
+        geojson.dump(geojson_feature_collection, outfile, indent=2)
+
     # for building in buildings:
     #     if building.area_square_meters > 1000:
     #         print(
@@ -100,14 +126,29 @@ def main():
     #         )
 
 
-def get_buildings_test_area(responseformat="json"):
-    query = """(
-        way[building](52.09261214198491,5.562860339734603,52.109061883798304,5.592364638898419);
+def get_buildings(bbox, exclude_types):
+    bbox = f"({bbox[0]},{bbox[1]},{bbox[2]},{bbox[3]});"
+    exclude_str = "|".join(exclude_types)
+    query = f"""(
+        way[building]["building"!~"^({exclude_str})$"]{bbox}
     )   
     """
+    print(f"get buildings for {bbox}")
     return api.get(
-        query, responseformat=responseformat, verbosity="geom"
+        query, responseformat="json", verbosity="geom"
     )  # use verbosity = geom to get way geometry in geojson
+
+
+def get_buildings_batches(bbox, exclude_types=("house", "apartments", "retail")):
+    tiles = generate_tiles(
+        bbox[0], bbox[1], bbox[2], bbox[3], delta_lat=0.07, delta_lon=0.07
+    )
+    print(f"{len(tiles)} tiles create")
+    buildings = []
+    for i, tile in enumerate(tiles):
+        print(f"getting tile: {i+1}/{len(tiles)}")
+        buildings += get_buildings(tile, exclude_types=exclude_types)["elements"]
+    return buildings
 
 
 def get_country_area_id():
@@ -123,6 +164,30 @@ def get_api_status():
 
 def kill_my_queries():
     return requests.get("https://overpass-api.de/api/kill_my_queries")
+
+
+def generate_tiles(min_lat, min_lon, max_lat, max_lon, delta_lat, delta_lon):
+    lat_lon_tiles = []
+    lat = min_lat
+    while lat < max_lat:
+        lon = min_lon
+        while lon < max_lon:
+            lat_lon_tiles.append((lat, lon, lat + delta_lat, lon + delta_lon))
+            lon += delta_lon
+        lat += delta_lat
+    return lat_lon_tiles
+
+
+def shapely_to_geojson(shapely_polygon: Polygon) -> geojson.Feature:
+    geojson_polygon = shapely.geometry.mapping(shapely_polygon)
+    return geojson.Feature(geometry=geojson_polygon)
+
+
+def shapely_collection_to_geojson(
+    shapely_polygons: List[Polygon],
+) -> geojson.FeatureCollection:
+    features = [shapely_to_geojson(polygon) for polygon in shapely_polygons]
+    return geojson.FeatureCollection(features)
 
 
 if __name__ == "__main__":
