b8826da1cd3e986ea17e452c8434d8f0b3d3815c
Bart Römgens <bart.romgens@gmail.com>
2024-07-16 21:06:43 +0200

    Add ngx-leaflet and standard map

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LeafletModule } from "@bluehalo/ngx-leaflet";
+import { circle, latLng, marker, polygon, tileLayer } from "leaflet";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LeafletModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'client';
+  title = 'livestockmap';
+  options = {
+    layers: [
+      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        { maxZoom: 18, attribution: '...' })
+    ],
+    zoom: 5,
+    center: latLng(46.879966, -121.726909)
+  };
+  layers = [
+    circle([ 46.95, -122 ], { radius: 5000 }),
+    polygon([[ 46.8, -121.85 ], [ 46.92, -121.92 ], [ 46.87, -121.8 ]]),
+    marker([ 46.879966, -121.726909 ])
+  ];
 }
