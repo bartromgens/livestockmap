10237d4b3cef7d53eb5763ddc91b3a5c1c16fcb3
Bart Römgens <bart.romgens@gmail.com>
2024-07-22 22:43:51 +0200

    Refactor: remove unused code

--- osm/overpass.py
+++ osm/overpass.py
@@ -1,10 +1,5 @@
-from typing import List
-
-import geojson
 import overpass
 import requests
-import shapely
-from shapely import Polygon
 
 api = overpass.API(timeout=60)
 
@@ -24,13 +19,13 @@ def kill_my_queries():
     return requests.get("https://overpass-api.de/api/kill_my_queries")
 
 
-def shapely_to_geojson(shapely_polygon: Polygon) -> geojson.Feature:
-    geojson_polygon = shapely.geometry.mapping(shapely_polygon)
-    return geojson.Feature(geometry=geojson_polygon)
-
-
-def shapely_collection_to_geojson(
-    shapely_polygons: List[Polygon],
-) -> geojson.FeatureCollection:
-    features = [shapely_to_geojson(polygon) for polygon in shapely_polygons]
-    return geojson.FeatureCollection(features)
+# def shapely_to_geojson(shapely_polygon: Polygon) -> geojson.Feature:
+#     geojson_polygon = shapely.geometry.mapping(shapely_polygon)
+#     return geojson.Feature(geometry=geojson_polygon)
+#
+#
+# def shapely_collection_to_geojson(
+#     shapely_polygons: List[Polygon],
+# ) -> geojson.FeatureCollection:
+#     features = [shapely_to_geojson(polygon) for polygon in shapely_polygons]
+#     return geojson.FeatureCollection(features)
