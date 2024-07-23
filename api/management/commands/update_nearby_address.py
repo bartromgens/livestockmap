f354665973663c855a3ae4019fa6604d7b0723cd
Bart Römgens <bart.romgens@gmail.com>
2024-07-23 23:08:12 +0200

    Search in a radius of 200 m in case no nearby addresses are found

--- api/management/commands/update_nearby_address.py
+++ api/management/commands/update_nearby_address.py
@@ -13,13 +13,16 @@ class Command(BaseCommand):
     help = ""
 
     def handle(self, *args, **options):
-        distance = 100
         buildings = Building.objects.all()
         for i, building in enumerate(buildings):
             self.stdout.write(f"finding address for building {i+1}/{len(buildings)}")
             nodes = get_address_nearby(
-                building.center.lat, building.center.lon, distance
+                building.center.lat, building.center.lon, distance=100
             )
+            if len(nodes) == 0:
+                nodes = get_address_nearby(
+                    building.center.lat, building.center.lon, distance=200
+                )
             addresses_nearby = [Address.get_or_create_from_node(node) for node in nodes]
             building.addresses_nearby.set(addresses_nearby)
             building.save()
