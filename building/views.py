25360166f4cc851d1d197cdbcba0c12d7144a254
Bart Römgens <bart.romgens@gmail.com>
2024-07-24 10:00:14 +0200

    Add nearby addresses to the building card

--- building/views.py
+++ building/views.py
@@ -1,13 +1,39 @@
 from rest_framework import serializers
 from rest_framework import viewsets
 
+from building.models import Address
 from building.models import Building
 
 
+class AddressSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Address
+        fields = [
+            "node_id",
+            "street",
+            "housenumber",
+            "postcode",
+            "city",
+            "lat",
+            "lon",
+        ]
+
+
 class BuildingSerializer(serializers.HyperlinkedModelSerializer):
+    addresses_nearby = AddressSerializer(many=True, read_only=True)
+
     class Meta:
         model = Building
-        fields = ["id", "way_id", "area", "length", "width", "tags", "geometry"]
+        fields = [
+            "id",
+            "way_id",
+            "area",
+            "length",
+            "width",
+            "tags",
+            "geometry",
+            "addresses_nearby",
+        ]
 
 
 class BuildingViewSet(viewsets.ModelViewSet):
--- client/src/app/core/building.ts
+++ client/src/app/core/building.ts
@@ -3,6 +3,16 @@ export interface CoordinateResource {
   lon: number;
 }
 
+export interface AddressResource {
+  "node_id": number;
+  "street": string;
+  "housenumber": string;
+  "postcode": string;
+  "city": string;
+  "lat": number;
+  "lon": number;
+}
+
 export interface BuildingResource {
   way_id: number;
   area: number;
@@ -10,6 +20,7 @@ export interface BuildingResource {
   width: number;
   tags: {[key: string]: string};
   geometry: CoordinateResource[];
+  addresses_nearby: AddressResource[];
 }
 
 export class Coordinate {
@@ -28,6 +39,39 @@ export class Coordinate {
   }
 }
 
+export class Address {
+  constructor(
+    public node_id: number,
+    public street: string,
+    public housenumber: string,
+    public postcode: string,
+    public city: string,
+    public lat: number,
+    public lon: number,
+  ) {
+  }
+
+  static fromResource(resource: AddressResource): Address {
+    return new Address(
+      Number(resource.node_id),
+      resource.street,
+      resource.housenumber,
+      resource.postcode,
+      resource.city,
+      Number(resource.lat),
+      Number(resource.lon)
+    )
+  }
+
+  static fromResources(resources: AddressResource[]): Address[] {
+    return resources.map(resource => Address.fromResource(resource));
+  }
+
+  toString(): string {
+    return `${this.street} ${this.housenumber} ${this.city}`
+  }
+}
+
 export class Building {
   constructor(
     public way_id: number,
@@ -36,12 +80,22 @@ export class Building {
     public width: number,
     public tags: {[key: string]: string},
     public geometry: Coordinate[],
+    public addresses_nearby: Address[]
   ) {
   }
 
   static fromResource(resource: BuildingResource): Building {
-    const coordinates = Coordinate.fromResources(resource.geometry);
-    return new Building(resource.way_id, resource.area, resource.length, resource.width, resource.tags, coordinates);
+    const coordinates: Coordinate[] = Coordinate.fromResources(resource.geometry);
+    const addresses_nearby: Address[] = Address.fromResources(resource.addresses_nearby);
+    return new Building(
+      resource.way_id,
+      resource.area,
+      resource.length,
+      resource.width,
+      resource.tags,
+      coordinates,
+      addresses_nearby
+    );
   }
 
   static fromResources(resources: BuildingResource[]): Building[] {
