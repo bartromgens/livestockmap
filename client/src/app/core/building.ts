acd6ac1e777329b00b5d61d4f86edcdeae17b72a
Bart Römgens <bart.romgens@gmail.com>
2024-07-26 21:48:25 +0200

    Sort building addresses by distance and show this distance

--- client/src/app/core/building.ts
+++ client/src/app/core/building.ts
@@ -21,6 +21,10 @@ export interface BuildingResource {
   tags: {[key: string]: string};
   geometry: CoordinateResource[];
   addresses_nearby: AddressResource[];
+  lon_min: number;
+  lon_max: number;
+  lat_min: number;
+  lat_max: number;
 }
 
 export class Coordinate {
@@ -36,6 +40,32 @@ export class Coordinate {
 
   static fromResources(resources: CoordinateResource[]): Coordinate[] {
     return resources.map(resource => Coordinate.fromResource(resource));
+  }
+
+  static toRadians(degrees: number): number {
+    return degrees * (Math.PI / 180);
+  }
+
+  distanceTo(coordinate: Coordinate): number {
+    return Coordinate.distance(this, coordinate);
+  }
+
+  /**
+   * The haversine distance between two coordinates
+   */
+  static distance(coord1: Coordinate, coord2: Coordinate): number {
+    const R = 6371 * 1000; // Radius of the Earth in meters
+
+    const dLat = this.toRadians(coord2.lat - coord1.lat);
+    const dLon = this.toRadians(coord2.lon - coord1.lon);
+    const rLat1 = this.toRadians(coord1.lat);
+    const rLat2 = this.toRadians(coord2.lat);
+
+    const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+              Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(rLat1) * Math.cos(rLat2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    return R * c; // Distance in kilometers
   }
 }
 
@@ -70,6 +100,10 @@ export class Address {
   toString(): string {
     return `${this.street} ${this.housenumber} ${this.city}`
   }
+
+  get coordinate(): Coordinate {
+    return new Coordinate(this.lat, this.lon);
+  }
 }
 
 export class Building {
@@ -80,13 +114,17 @@ export class Building {
     public width: number,
     public tags: {[key: string]: string},
     public geometry: Coordinate[],
-    public addresses_nearby: Address[]
+    public addresses_nearby: Address[],
+    public center: Coordinate
   ) {
+    this.addresses_nearby.sort((a: Address, b: Address) => a.coordinate.distanceTo(this.center) - b.coordinate.distanceTo(this.center))
   }
 
   static fromResource(resource: BuildingResource): Building {
     const coordinates: Coordinate[] = Coordinate.fromResources(resource.geometry);
     const addresses_nearby: Address[] = Address.fromResources(resource.addresses_nearby);
+    const lat = Math.abs(resource.lat_max - resource.lat_min) / 2 + resource.lat_min
+    const lon = Math.abs(resource.lon_max - resource.lon_min) / 2 + resource.lon_min
     return new Building(
       resource.way_id,
       resource.area,
@@ -94,7 +132,8 @@ export class Building {
       resource.width,
       resource.tags,
       coordinates,
-      addresses_nearby
+      addresses_nearby,
+      new Coordinate(lat, lon)
     );
   }
 
