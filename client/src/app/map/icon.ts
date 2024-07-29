946778a7df8dac583abe6cdfc7c6480f04d4038b
Bart Römgens <bart.romgens@gmail.com>
2024-07-29 09:23:47 +0200

    Add transparency and color to animal icons

--- client/src/app/map/icon.ts
+++ client/src/app/map/icon.ts
@@ -5,18 +5,18 @@ const ICON_SIZE_DEFAULT = 24;
 export const chickenIcon = icon({
   iconUrl: "assets/chicken.png",
   iconSize: [ICON_SIZE_DEFAULT, ICON_SIZE_DEFAULT], // size of the icon
-  iconAnchor: [0, 0], // point of the icon which will correspond to marker's location
+  iconAnchor: [-ICON_SIZE_DEFAULT/2, 0], // point of the icon which will correspond to marker's location
   // popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
 });
 
 export const pigIcon = icon({
   iconUrl: "assets/pig.png",
   iconSize: [ICON_SIZE_DEFAULT, ICON_SIZE_DEFAULT], // size of the icon
-  iconAnchor: [ICON_SIZE_DEFAULT, 0], // point of the icon which will correspond to marker's location
+  iconAnchor: [ICON_SIZE_DEFAULT/2, 0], // point of the icon which will correspond to marker's location
 });
 
 export const cowIcon = icon({
   iconUrl: "assets/cow.png",
   iconSize: [ICON_SIZE_DEFAULT, ICON_SIZE_DEFAULT], // size of the icon
-  iconAnchor: [-ICON_SIZE_DEFAULT, 0], // point of the icon which will correspond to marker's location
+  iconAnchor: [0, ICON_SIZE_DEFAULT * 0.7], // point of the icon which will correspond to marker's location
 });
