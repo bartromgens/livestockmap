3b8f81a00ab3e3af6225c8c20a9b1108922abf0b
Bart Römgens <bart.romgens@gmail.com>
2024-07-29 17:48:53 +0200

    Limit number of addresses to find companies for

--- api/management/commands/update_companies.py
+++ api/management/commands/update_companies.py
@@ -3,6 +3,7 @@ import logging
 from django.core.management.base import BaseCommand
 
 from building.models import Address
+from building.models import Company
 
 logger = logging.getLogger(__name__)
 
@@ -11,5 +12,11 @@ class Command(BaseCommand):
     help = ""
 
     def handle(self, *args, **options):
-        addresses = Address.objects.all()
-        Address.update_companies(addresses)
+        addresses = (
+            Address.objects.filter(building__addresses_nearby__isnull=False)
+            .filter(company__isnull=True)
+            .distinct()
+        )
+        logger.info(f"found {len(addresses)}/{Address.objects.all().count()} addresses")
+        companies = Address.update_companies(addresses)
+        Company.update_types(companies)
