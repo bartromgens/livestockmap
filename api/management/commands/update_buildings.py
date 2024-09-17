be2e98a9f9dfc2244424f059581a32a580d1d1da
Bart Römgens <bart.romgens@gmail.com>
2024-09-17 19:51:38 +0200

    Reduce memory usage of update_buildings command even more

--- api/management/commands/update_buildings.py
+++ api/management/commands/update_buildings.py
@@ -1,7 +1,6 @@
 import logging
 
 from django.core.management.base import BaseCommand
-from django.core.paginator import Paginator
 
 from building.models import Building
 
@@ -12,22 +11,14 @@ class Command(BaseCommand):
     help = "Update all buildings with derived information such as company."
 
     def handle(self, *args, **options):
-        buildings = Building.objects.all().order_by('id')
-        # logger.info(f"found {len(buildings)} buildings")
-
-        page_size = 100
-        paginator = Paginator(buildings, page_size)
+        building_count = Building.objects.all().count()
+        logger.info(f"found {building_count} buildings")
 
         i = 0
-        for p_count in paginator.page_range:
-            i += page_size
+        for building in Building.objects.all().iterator(chunk_size=1000):
+            i += 1
             logger.info(
-                f"update building {i}/{len(buildings)}: {(i / len(buildings) * 100):.2f}%"
+                f"update building {i}/{building_count}: {(i / building_count * 100):.2f}%"
             )
-            page = paginator.page(p_count)
-            for building in page.object_list:
-                building.update_company(save=False)
-
-            # Perform bulk update for the current page
-            Building.objects.bulk_update(page.object_list, ['company'])
+            building.update_company(save=True)
 
