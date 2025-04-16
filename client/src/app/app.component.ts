d53947e927f210aff5ca31356d1f535d8f00768e
Bart Römgens <bart.romgens@gmail.com>
2025-04-16 14:41:17 +0200

    Add about and code navbar items

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatToolbarModule, NgOptimizedImage],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    RouterLink,
+    MatToolbarModule,
+    MatButtonModule,
+    NgOptimizedImage,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
