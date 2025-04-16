3f479971936cdb26246b1d891b46797be4ccd039
Bart Römgens <bart.romgens@gmail.com>
2025-04-16 13:16:50 +0200

    Fix missing NgOptimizedImage import

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatToolbarModule],
+  imports: [CommonModule, RouterOutlet, MatToolbarModule, NgOptimizedImage],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
