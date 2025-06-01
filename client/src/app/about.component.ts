a2eba46c72b67bbec027891abd8b8cffa88b5948
Bart Römgens <bart.romgens@gmail.com>
2025-06-01 21:40:18 +0200

    Add a button on about page to go back to the map

--- client/src/app/about.component.ts
+++ client/src/app/about.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-about',
   standalone: true,
+  imports: [CommonModule, RouterLink, MatButtonModule, MatIconModule],
   templateUrl: './about.component.html',
 })
 export class AboutComponent {}
